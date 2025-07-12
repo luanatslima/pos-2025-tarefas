0691bed2cf15c20da607bb641d73232a86c38304
luanatslima <luanatayna2007@gmail.com>
2025-07-12 10:24:05 -0300

    soap.py OK

--- soap/soap.py
+++ soap/soap.py
@@ -1,3 +1,38 @@
-import soap
+import zeep
+from xml.dom import minidom
 
-api_url = ""
+wsdl_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL"
+client = zeep.Client(wsdl=wsdl_url)
+
+# MOEDA DO PAÍS
+def get_currency(country_code):
+    result = client.service.CountryCurrency(country_code)
+    print(f"{result.sName} ({result.sISOCode})")
+
+# BANDEIRA DO PAÍS
+def get_flag(country_code):
+    result = client.service.CountryFlag(country_code)
+    print(result)
+
+# NOME DO PAÍS
+def get_country_name(country_code):
+    result = client.service.CountryName(country_code)
+    print(result)
+
+# MENU DE OPÇÕES
+print("Escolha uma opção:")
+print("1 - Moeda do país")
+print("2 - Bandeira do país")
+print("3 - Nome completo do país")
+
+opcao = input("Digite o número da opção: ")
+codigo = input("Digite o código do país (ex: BR, US, JP): ").upper()
+
+if opcao == '1':
+    get_currency(codigo)
+elif opcao == '2':
+    get_flag(codigo)
+elif opcao == '3':
+    get_country_name(codigo)
+else:
+    print("Opção inválida.")
--- soap/zeep.py
+++
@@ -1,4 +0,0 @@
-import zeep
-
-wsdl_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL"
-
